4d181df016def54559e722eea3c834887f497f62
kylebaker <kylethomasbaker@gmail.com>
2019-10-28 00:17:48 -0600

    Initial Push

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,29 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName    = 'wireguard'
+$fileType       = 'msi'
+$url64          = "https://download.wireguard.com/windows-client/wireguard-amd64-0.0.35.msi"
+$checksum64     = ''
+$checksumType64 = 'sha256'
+$url            = "https://download.wireguard.com/windows-client/wireguard-x86-0.0.35.msi"
+$checksum       = ''
+$checksumType   = 'sha256'
+$silentArgs     = '/qn'
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+
+$packageArgs = @{
+    packageName     = $packageName
+    fileType        = $fileType
+    url             = $url
+    url64           = $url64
+    checksum        = $checksum
+    checksum64      = $checksum64
+    checksumType    = $checksumType
+    checksumType64  = $checksumType64
+    validExitCodes  = $validExitCodes
+    silentArgs      = $silentArgs
+
+}
+
+Install-ChocolateyPackage @packageArgs
